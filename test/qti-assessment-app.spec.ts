67b5df1837488ad7f3b05c82bedd2fa234e6f4c4
derfeingeist <103456202+derfeingeist@users.noreply.github.com>
2022-12-04 11:32:33 +0000

    rewrite test

--- test/qti-assessment-app.spec.ts
+++ test/qti-assessment-app.spec.ts
@@ -1,6 +1,8 @@
-import { test, expect } from 'vitest';
+import { test, expect, describe, it } from 'vitest';
 import { QtiAssessmentApp } from '../src/qti-assessment-app';
 
-test('', () => {
-  expect(QtiAssessmentApp).toBeDefined();
+describe('Assessment app', () => {
+  it('should be defined', () => {
+    expect(QtiAssessmentApp).toBeDefined();
+  });
 });
